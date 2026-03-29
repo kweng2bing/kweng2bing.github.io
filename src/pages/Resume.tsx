f4158b71f66ab59d9aeef18e75e53232f90dbf7d
Kalvin W <kweng2@binghamton.edu>
2026-03-28 21:55:01 -0400

    Adding Resume and Github Workflows

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -75,7 +75,7 @@ const Resume = () => {
           </h1>
         </div>
         <a
-          href="/resume.pdf"
+          href="assets/resume.pdf"
           download
           className="rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground transition-colors duration-100 hover:bg-primary/90"
         >
